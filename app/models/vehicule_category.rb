4d89aff0fb9cc036fe798d4227deae2b3ddb8c0d
David Hernandez Azagury <davidhazagury@gmail.com>
2020-08-27 16:25:39 +0200

    Vehicule category model/table created

---
+++ app/models/vehicule_category.rb
@@ -0,0 +1,2 @@
+class VehiculeCategory < ApplicationRecord
+end
---
+++ test/models/vehicule_category_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class VehiculeCategoryTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
