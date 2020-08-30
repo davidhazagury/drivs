eb2bd2ccc69a6695e420d94759f9f3e420b2f3f0
David Hernandez Azagury <davidhazagury@gmail.com>
2020-08-30 09:16:44 +0200

    Validations for bookings set

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,10 @@
 class Booking < ApplicationRecord
+
+  #MODEL VALIDATIONS
+  validates :start_date, presence: true
+  validates :end_time, presence: true
+
+  # DB RELATIONSHIPS
   has_many :requested_bookings
   belongs_to :vehicule
 end
