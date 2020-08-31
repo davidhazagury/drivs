42e10ade18e547cd469a66d91c36bf51bc36d870
David Hernandez Azagury <davidhazagury@gmail.com>
2020-08-31 18:54:45 +0200

    Fixing bugs

--- app/controllers/vehicules_controller.rb
+++ app/controllers/vehicules_controller.rb
@@ -11,7 +11,18 @@ class VehiculesController < ApplicationController
   end
 
   def create
-    raise
+    @vehicule = Vehicule.new(vehicule_params)
+    authorize @vehicule
+    @vehicule.user_id = current_user.id
+    if @vehicule.save!
+      redirect_to root
+    else
+      render :new
+    end
   end
 
+  private
+  def vehicule_params
+    params.require(:vehicule).permit(:brand, :location, :features, :photo, :capacity, :price, :vehicule_category)
+  end
 end
