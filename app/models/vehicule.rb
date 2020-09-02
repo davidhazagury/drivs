3e8540e7144ef7a2fd29ba95da2b5de3f0a3b174
David Hernandez Azagury <davidhazagury@gmail.com>
2020-09-02 17:47:42 +0200

    Fixed the massive bug. Lat and Long showing in the vehicules

--- app/models/vehicule.rb
+++ app/models/vehicule.rb
@@ -13,4 +13,10 @@ class Vehicule < ApplicationRecord
 
   # CLOUDINARY PHOTOS OF THE VEHICULE
   has_many_attached :photos
+
+  # GEOCODING
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
+
+
 end
--- db/migrate/20200831170100_drop_vehiculecategories.rb
+++
@@ -1,5 +0,0 @@
-class DropVehiculecategories < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :vehicule_categories
-  end
-end
