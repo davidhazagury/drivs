1d33fbbd5564ef7cd83216b0874c842263acb45e
David Hernandez Azagury <davidhazagury@gmail.com>
2020-09-03 16:26:50 +0200

    Search bar working with PG gem

--- app/models/vehicule.rb
+++ app/models/vehicule.rb
@@ -18,5 +18,11 @@ class Vehicule < ApplicationRecord
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
 
-
+  # SEARCHING PG_SEARCH
+  include PgSearch::Model
+  pg_search_scope :search_by_brand_and_location,
+    against: [ :brand, :location ],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
 end
