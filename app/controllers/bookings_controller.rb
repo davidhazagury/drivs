ba6aa7d5ab544d99c301d8313aadf2c56572e342
David Hernandez Azagury <davidhazagury@gmail.com>
2020-10-06 08:58:33 +0200

    WORKING ON THE STARTING DATE NEEDS TO BE AFTER THE ENDING DATE.

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,3 +1,4 @@
+require 'date'
 class BookingsController < ApplicationController
 
   def new
@@ -7,7 +8,22 @@ class BookingsController < ApplicationController
   end
 
   def create
-    raise
+
+    @booking = Booking.new(strong_params)
+    @vehicule = Vehicule.find(params[:vehicule_id])
+    @booking.user_id = current_user.id
+    @booking.vehicule_id = @vehicule.id
     authorize @booking
+    # IF END DATE BIGGER THAN START DATE => RAISE AN ERROR
+    # Need to transform string into date format
+    if @booking.start_date < @booking.end_time
+      redirect_to new_vehicule_booking_path(@vehicule), notice: 'Starting date can´t be bigger than ending date'
+    end
+  end
+
+  private
+
+  def strong_params
+    params.require(:booking).permit(:start_date, :end_time)
   end
 end
