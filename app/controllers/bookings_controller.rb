5fd3ac895c5bc8805674f81ec0bbf88dcbe75083
David Hernandez Azagury <davidhazagury@gmail.com>
2020-10-06 07:45:50 +0200

    Simple form for added to view-new-booking page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,11 +1,13 @@
 class BookingsController < ApplicationController
 
   def new
+    @vehicule = Vehicule.find(params[:vehicule_id])
     @booking = Booking.new
     authorize @booking
   end
 
   def create
-
+    raise
+    authorize @booking
   end
 end
